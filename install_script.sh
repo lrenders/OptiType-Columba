d2397be1228c638255c27e64b4ec4e8daab6971e
lrenders <45067715+lrenders@users.noreply.github.com>
2025-07-09 14:27:19 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -13,16 +13,13 @@ else
     exit 1
 fi
 
-# pull the latest Columba image
+# pull the latest Columba repo
 echo "Installing Columba..."
 git clone https://github.com/biointec/columba.git columba-src \
     && cd columba-src \
-    && git checkout v2.0.2 \
     && bash build_script.sh Vanilla \
     && build_Vanilla/columba_build -f ../data/hla_reference_dna.fasta \
     && build_Vanilla/columba_build -f ../data/hla_reference_rna.fasta  \
     && mv build_Vanilla/columba ../columba \
     && cd .. \
     && rm -rf columba-src
-
-# 
